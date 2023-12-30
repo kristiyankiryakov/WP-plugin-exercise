27f22dfb6cb6ec2160a6ff92a2a41cbf45b591b4
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2023-12-30 03:14:39 -0800

    uninstall file

--- plugins/kris-plugin/uninstall.php
+++ plugins/kris-plugin/uninstall.php
@@ -9,3 +9,15 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
+
+
+// $books = get_posts(array('post_type' => 'book', 'numberposts' => -1));
+
+// foreach ($books as $book) {
+    //     wp_delete_post($book->ID, true);
+    // }
+    
+global $wpdb;
+
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book' " );
+$wpdb->query("DELETE FROM  wp_postmeta WHERE post_id NOT IN (SELECT id from  wp_posts) ");
