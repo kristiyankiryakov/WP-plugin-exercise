c0000c0eb36df12fdc44a1e8eee7ba0bf6319435
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2023-12-30 14:57:56 +0200

    add page and link to it

--- plugins/kris-plugin/kris-plugin.php
+++ plugins/kris-plugin/kris-plugin.php
@@ -20,39 +20,50 @@ if (!defined('ABSPATH')) {
 
 class KrisPlugin
 {
-    function __construct()
+    public $plugin;
+    public function __construct()
     {
-        // adds the function to the class otherwise you can't access it
-        add_action('init', array($this, 'custom_post_type'));
+        $this->plugin = plugin_basename(__FILE__);
     }
-
-    function activate()
-    {
-        $this->custom_post_type();
-        flush_rewrite_rules();
-    }
-    function register()
+    public function register()
     {
         // admin_enqueue_scripts for backend and wp_enqueue_scripts for frontend
         add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+
+        add_action('admin_menu', array($this, 'add_admin_pages'));
+
+        add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
     }
-    function deactivate()
+    public function settings_link($links)
     {
-        flush_rewrite_rules();
+        $settings_link = '<a href="admin.php?page=kris_plugin">Settings</a>';
+        array_push($links, $settings_link);
+        return $links;
     }
-    function uninstall()
+    public function add_admin_pages()
     {
-        // delete CPT
-        // delete all plugin data from the DB
+        add_menu_page('Kris Plugin', 'Kris', 'manage_options', 'kris_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
     }
+    public function admin_index()
+    {
+        require_once plugin_dir_path(__FILE__) . 'templates/admin.php';
+    }
+    protected function create_post_type()
+    {
+        add_action('init', array($this, 'custom_post_type'));
+    }
+
     function custom_post_type()
     {
         register_post_type('book', ['public' => true, 'label' => 'Books']);
     }
     function enqueue()
     {
+        // enqueue all our scripts
         wp_enqueue_style('mypluginstyle', plugins_url('/assets/mystyle.css', __FILE__));
         wp_enqueue_script('myscript', plugins_url('/assets/myscript.js', __FILE__));
+        // syntax to enqueue async script
+        // wp_enqueue_script('myscript', plugins_url('/assets/myscript.js', __FILE__), null, null, ['strategy' => 'async']);
     }
 }
 
@@ -63,9 +74,9 @@ if (class_exists('KrisPlugin')) {
 
 
 // activation
-register_activation_hook(__FILE__, array($krisPlugin, 'activate'));
+// register_activation_hook(__FILE__, array($krisPlugin, 'activate'));
 
 // deactivate
-register_deactivation_hook(__FILE__, array($krisPlugin, 'deactivate'));
+// register_deactivation_hook(__FILE__, array($krisPlugin, 'deactivate'));
 
 // uninstall
---
+++ plugins/kris-plugin/templates/admin.php
@@ -0,0 +1,3 @@
+<h1>
+    Kris Plugin
+</h1>
