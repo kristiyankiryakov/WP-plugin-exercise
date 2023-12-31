b95bb16a3b6a654fb22d7e9e3c3df39f4372b3fc
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2023-12-31 10:58:28 +0200

    flush data on activate and deactivate

--- plugins/kris-plugin/kris-plugin.php
+++ plugins/kris-plugin/kris-plugin.php
@@ -14,6 +14,9 @@ License: GPLv2 or later
 Text Domain: kris-plugin 
 */
 
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
 if (!defined('ABSPATH')) {
     die;
 }
@@ -25,6 +28,19 @@ if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_URL', plugin_dir_url(__FILE__));
 define('PLUGIN', plugin_basename(__FILE__));
+
+function activate()
+{
+    Activate::activate();
+}
+
+function deactivate()
+{
+    Deactivate::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate');
+register_deactivation_hook(__FILE__, 'deactivate');
 
 if (class_exists('Inc\\Init')) {
     Inc\Init::register_services();
