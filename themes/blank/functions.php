083d5466aa67ceeb7dec0ca74925b2402c1c353d
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-01-06 15:28:09 +0200

    display posts

--- themes/blank/functions.php
+++ themes/blank/functions.php
@@ -1,6 +1,13 @@
 <?php
-function register_my_menu() {
+function register_my_menu()
+{
     register_nav_menu('header-menu', __('Header Menu'));
 }
+function enqueue_theme_styles()
+{
+    wp_enqueue_style('theme-style', get_stylesheet_uri());
+}
 add_action('after_setup_theme', 'register_my_menu');
+add_theme_support('post-thumbnails');
+add_action('wp_enqueue_scripts', 'enqueue_theme_styles');
 
---
+++ themes/blank/template-blog.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Template Name: Blog Page
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <header class="page-header">
+            <h1 class="page-title">
+                <?php echo get_the_title(); ?>
+            </h1>
+        </header>
+
+        <div class="posts-list">
+            <?php
+            $args = array(
+                'post_type' => 'post',
+            );
+
+            $query = new WP_Query($args);
+
+            if ($query->have_posts()):
+                while ($query->have_posts()):
+                    $query->the_post();
+                    ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class('blog-post'); ?>>
+                        <?php if (has_post_thumbnail()): ?>
+                            <div class="post-thumbnail">
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                    <?php the_post_thumbnail(); ?>
+                                </a>
+                            </div>
+                        <?php endif; ?>
+                        <div class="entry-content">
+                            <h2 class="entry-title">
+                                <a href="<?php the_permalink(); ?>" rel="bookmark">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </article>
+                    <?php
+                endwhile;
+            else:
+                get_template_part('template-parts/content', 'none');
+            endif;
+            wp_reset_postdata();
+            ?>
+        </div>
+    </main>
+</div>
+
+<?php get_footer(); ?>
